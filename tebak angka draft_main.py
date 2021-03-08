5d850eb6b6c4fb0a9435f72a23c4d7747ac78e5c
ariqyusa <49482187+ariqyusa@users.noreply.github.com>
2021-03-08 10:40:23 +0700

    Update tebak angka draft_main.py

--- tebak angka draft_main.py
+++ tebak angka draft_main.py
@@ -1,24 +1,51 @@
-#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Thu Mar  4 06:33:40 2021
+Created on Mon Mar  8 03:56:34 2021
 
-@author: sheenaabigail
+@author: sheenaabigail, ariqyusa
 """
 import random
 
-def main():
+def mainMenu():
+    print('''
+    ------------------------------
+              MAIN MENU 
+              
+    Input 1-4 
+    1. Play 
+    2. Register
+    3. High Score
+    4. Exit
+    ''')
+    
+    selection = str(input("Enter choice : "))
+    if selection == "1" : 
+        play()
+        input('Enter any key to continue')
+        mainMenu()
+    elif selection == "2" : 
+        print(selection)
+    elif selection == "3" : 
+        print(selection) 
+    elif selection == "4" : 
+        print("Thank you for playing!" )
+    else :
+        print("Invalid choice. Enter 1-4 ")
+        mainMenu()
+        
+def play():
     score = 100
     angka = random.randint(1, 20)
     tebakan = 0
 
-    print("**************************************")
-    print("PERMAINAN TEBAK ANGKA")
-    print("**************************************")
-    print('Saya memikirkan sebuah angka dari 1-20')
-    print('Silahkan tebak angka yang saya pikirkan')
-    print('Input harus berupa ANGKA')
-    print(f'Score Anda saat ini adalah {score}')
+    print('''
+          **************************************
+                   PERMAINAN TEBAK ANGKA
+          **************************************
+          Saya memikirkan sebuah angka dari 1-20
+          Silahkan tebak angka yang saya pikirkan
+          Input harus berupa ANGKA''')
+    print(f'''Score Anda saat ini adalah {score}''')
 
     while tebakan != angka:
         tebakan = input('\nMasukkan tebakan Anda: ')
@@ -27,21 +54,31 @@ def main():
             print("Tebakan harus berupa angka.")
             print("Penalty, poin dikurangi 20")
             print(f"Score anda saat ini {score}")
+            if score <= 0:
+                break
             continue
         tebakan = int(tebakan)
         if tebakan > angka:
             score -= 10
             print('Tebakan Anda terlalu besar, score Anda dikurangi 10')
             print(f"Score anda saat ini {score}")
+            if score <= 0 : 
+                break
         elif tebakan < angka:
             score -= 10
             print('Tebakan Anda terlalu kecil, score Anda dikurangi 10')
             print(f"Score anda saat ini {score}")
+            if score <= 0 : 
+                break
             
-    print('Selamat! Tebakan Anda benar')
-    print(f'Score Anda: {score}')
-        
+    print('-'*25)
+    if score <= 0 : 
+        print('Score Anda sudah habis' )
+        print('Silahkan coba kembali' )
+    if score != 0 :     
+        print('Selamat! Tebakan Anda benar')
+        print(f'Score Anda: {score}')
+    
+    
+mainMenu()
 
-if __name__ == "__main__":
-    main()
-            
